0fa4597fd9a2c3478ff0375f312d1a89086862e2
Kabita <pradhankabita7873@gmail.com>
2023-07-22 10:42:57 +0530

    This phase js add for login form validation

--- js/index.js
+++ js/index.js
@@ -7,24 +7,53 @@ function clikToLoginModal(){
 }
 function submitLoginForm(e){
     var obj = {username : '' , password : ''}
+    
+    var usnm = true;
+    var pss = true;
+    if(document.loginForm.username.value == ""){
+        usnm = false;
+        document.getElementById("username-error-span").style.display = "block";
+    }
+    if(document.loginForm.password.value == ""){
+        pss = false;
+        document.getElementById("password-error-span").style.display = "block";
+    } 
     console.log("username :" , document.loginForm.username.value)
-    obj.username = document.loginForm.username.value;
-    obj.password = document.loginForm.password.value;
-    console.log(loginAPI)
-    const xhttp = new XMLHttpRequest();
-    // xhttp.open("POST", "http://localhost:8080/api/auth/login");
-    xhttp.open("POST", loginAPI);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(JSON.stringify(obj));
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            const objects = JSON.parse(this.responseText);
-            console.log(objects);
+    console.log("pass :" , document.loginForm.password.value)
+    if(usnm == false || pss == false){
+        return false;
+    } else {
+        obj.username = document.loginForm.username.value;
+        obj.password = document.loginForm.password.value;
+        getJJsp("POST" , loginAPI , JSON.stringify(obj) , loginCallback);
+        return false;
+        // const xhttp = new XMLHttpRequest();
+        // xhttp.open("POST", loginAPI);
+        // xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        // xhttp.send(JSON.stringify(obj));
+        // xhttp.onreadystatechange = function () {
+        //     if (this.readyState == 4 && this.status == 200) {
+        //         const objects = JSON.parse(this.responseText);
+        //         console.log(objects);
+        //         localStorage.setItem("user", JSON.stringify(objects.user));
+        //         // document.location.href = "admin.html";
+        //     }
+        // };
+    }
+    
+}
+function loginCallback(){
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        const objects = JSON.parse(xhttp.responseText);
+        console.log(objects);
+        if(objects.user){
             localStorage.setItem("user", JSON.stringify(objects.user));
             document.location.href = "admin.html";
+        } else {
+            document.getElementById('login-whole-error').style.display = "block"
         }
-    };
-    return false;
+        
+    }
 }
 function allPostsApix(){
     getJJsp('GET',allPostsApi , null , callbackGetPost)
