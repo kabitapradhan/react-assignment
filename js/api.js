dbd2491e245efeb0dffb99a20b53e50f9cfe852c
Kabita <pradhankabita7873@gmail.com>
2023-07-23 18:44:43 +0530

    This phase i have done some design

--- js/api.js
+++ js/api.js
@@ -6,6 +6,8 @@ var addPostApi = "https://spring-assignment-production.up.railway.app/api/v1/pos
 var deletePostApi = "https://spring-assignment-production.up.railway.app/api/v1/post/";
 var allPostsApi = "https://spring-assignment-production.up.railway.app/api/v1/post/user/1";
 
+var allContactApi = "https://spring-assignment-production.up.railway.app/api/v1/contacts/";
+var RegisterContactApi = "https://spring-assignment-production.up.railway.app/api/v1/contacts/";
 // getJSON(post,loginAPI, "id=xxx&p1=yyy", callbcak funxtion);
 // function getJJsp(method ,path, params, callback) {
 // 	var ts = new Date().getTime();	
@@ -41,14 +43,17 @@ function checkLogedIn(){
         return false;
     }
 }
+function closePostUpdateModal(){
+  document.getElementById("postUpdateModal").style.display = "none";
+}
 function designPostUpload(title, content , postId){
     var cnt = "";
-    cnt += "<div style=\"display: block;z-index: 99999999;background: #0000004a;\" class=\"modal\" id=\"postModal\" >"
+    cnt += "<div style=\"display: block;z-index: 99999999;background: #0000004a;\" class=\"modal\" id=\"postUpdateModal\" >"
     + "<div class=\"modal-dialog\" role=\"document\">"
       + "<div class=\"modal-content\">"
         + "<div class=\"modal-header\">"
           + "<h5 class=\"modal-title\" id=\"exampleModalLabel\">Update Post</h5>"
-          + "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">"
+          + "<button onclick=\"closePostUpdateModal()\" type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">"
             + "<span aria-hidden=\"true\">&times;</span>"
           + "</button>"
         + "</div>"
@@ -72,36 +77,41 @@ function designPostUpload(title, content , postId){
 + "</div>"
 return cnt;
 }
+function closePostAddModal(){
+  document.getElementById("postAddModal").style.display = "none";
+}
 function designPostAdd(){
     var cnt = "";
-    cnt += "<div style=\"display: block;z-index: 99999999;background: #0000004a;\" class=\"modal\" id=\"postModal\" >"
+    cnt += "<div style=\"display: block;z-index: 99999999;background: #0000004a;\" class=\"modal\" id=\"postAddModal\" >"
     + "<div class=\"modal-dialog\" role=\"document\">"
       + "<div class=\"modal-content\">"
         + "<div class=\"modal-header\">"
           + "<h5 class=\"modal-title\" id=\"exampleModalLabel\">Update Post</h5>"
-          + "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">"
+          + "<button onclick=\"closePostAddModal()\" type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">"
             + "<span aria-hidden=\"true\">&times;</span>"
           + "</button>"
         + "</div>"
         + "<div class=\"modal-body\">"
-          + "<form name=\"addPostForm\" onsubmit=\"return posAddSubmit();\">"
+          + "<form name=\"addPostForm\" onsubmit=\"false;\">"
          
             + "<div class=\"form-group\">"
               + "<label for=\"exampleInputEmail1\">Title</label>"
               + "<input name=\"title\" type=\"text\" class=\"form-control\" id=\"exampleInputEmail1\" aria-describedby=\"emailHelp\" placeholder=\"Enter title\">"
-            + "</div>"
+              + "<span id=\"title-error-span\">Title must be need</span>"
+              + "</div>"
 
             + "<div class=\"form-group\">"
               + "<label for=\"exampleInputPassword1\">Content</label>"
               + "<input name=\"content\" type=\"text\" class=\"form-control\" id=\"exampleInputPassword1\" placeholder=\"content\">"
+              + "<span id=\"content-error-span\">Content must be need</span>"
             + "</div>"
 
             // + "<div class=\"form-group\">"
-            //   + "<label for=\"exampleInputPassword1\">Content</label>"
-            //   + "<input name=\"content\" type=\"text\" class=\"form-control\" id=\"exampleInputPassword1\" placeholder=\"content\">"
+            //   + "<label for=\"exampleFormControlFile1\">Upload post image</label>"
+            //   + "<input name=\"filesx\" onchange=\"uploadPostImage(event)\" type=\"file\" class=\"form-control-file\" id=\"exampleFormControlFile1\">"
             // + "</div>"
 
-            + "<button type=\"submit\" class=\"btn btn-primary\">Submit</button>"
+            + "<button type=\"button\" onclick=\"posAddSubmit()\" class=\"btn btn-primary\">Submit</button>"
           + "</form>"
         + "</div>"
        
--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ function loginCallback(){
         console.log(objects);
         if(objects.user){
             localStorage.setItem("user", JSON.stringify(objects.user));
-            document.location.href = "admin.html";
+            document.location.href = "posts.html";
         } else {
             document.getElementById('login-whole-error').style.display = "block"
         }
@@ -74,4 +74,68 @@ function callbackGetPost(){
             document.getElementById("marquee-post-container").style.display = "none";
         }
     }
+}
+function submitEnquiryForm(){
+    console.log("submitEnquiryForm calling")
+    // enquiryForm
+    var obj = {name : '' , phone : '',email : '' , message : ''}
+    var nm = true;
+    var phn = true;
+    var eml = true;
+    var msg = true;
+    if(document.enquiryForm.name.value == ""){
+        nm = false;
+        //document.getElementById("cname-error-span").style.display = "block";
+    }
+    if(document.enquiryForm.phone.value == ""){
+        phn = false;
+        //document.getElementById("cphone-error-span").style.display = "block";
+    } 
+    if(document.enquiryForm.email.value == ""){
+        eml = false;
+        //document.getElementById("cemail-error-span").style.display = "block";
+    }
+    if(document.enquiryForm.message.value == ""){
+        msg = false;
+        //document.getElementById("cmessage-error-span").style.display = "block";
+    } 
+    if(nm == false || phn == false || eml == false || msg == false){
+        console.log("hhhhhhhh")
+        
+    } else {
+        console.log("ffffff")
+        obj.name = document.enquiryForm.name.value;
+        obj.phone = document.enquiryForm.phone.value;
+        obj.email = document.enquiryForm.email.value;
+        obj.message = document.enquiryForm.message.value;
+        //console.log(obj)
+        getJJsp("POST" , RegisterContactApi , JSON.stringify(obj) , contactCallback);
+        
+    }
+}
+function contactCallback(){
+    console.log("contactCallback")
+    console.log(xhttp.readyState)
+    console.log(xhttp.status)
+    if (xhttp.readyState == 4 && (xhttp.status == 200 || xhttp.status == 201)  ) {
+        let objects = JSON.parse(this.responseText);
+        console.log(objects);
+        console.log(objects.email);
+        if(objects.email){
+            console.log("comming")
+            document.enquiryForm.name.value = "";
+            document.enquiryForm.phone.value = "";
+            document.enquiryForm.email.value = "";
+            document.enquiryForm.message.value = "";
+            document.getElementById('contact-succuss-modal').style.display = "block";
+            setTimeout(closeContactEnquiryModal, 5000);
+        }
+    }
+}
+function closeContactEnquiryModal(){
+    console.log("closeContactEnquiryModal call")
+    document.getElementById('contact-succuss-modal').style.display = "none";
+}
+function closeLoginModal(){
+    document.getElementById("userLoginModal").style.display = "none";
 }
